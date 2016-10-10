9441385af7cacb5d8c2ed9b77e9be87bd3a2f06e
Kiven Wang <984768569@qq.com>
2016-10-10 15:38:55 +0800

    no message

---
+++ kutils/src/main/java/com/kiven/kutils/tools/KAlertDialogHelper.java
@@ -0,0 +1,17 @@
+package com.kiven.kutils.tools;
+
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
+
+/**
+ *
+ * Created by kiven on 2016/10/10.
+ */
+
+public class KAlertDialogHelper {
+    public static void ShowSingleBtnAlertDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+
+    }
+}

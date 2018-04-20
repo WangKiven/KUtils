bec93a8a49c921f50b00fc7e47529ecfa1893466
kivenspc\wangkiven <984768569@qq.com>
2018-04-20 13:50:15 +0800

    no message

--- sample/src/main/java/com/kiven/sample/AHSmallAction.kt
+++ sample/src/main/java/com/kiven/sample/AHSmallAction.kt
@@ -1,18 +1,14 @@
 package com.kiven.sample
 
+import android.Manifest
 import android.app.ActivityManager
 import android.app.WallpaperManager
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import android.os.Build
 import android.os.Bundle
-import android.support.animation.DynamicAnimation
-import android.support.animation.SpringAnimation
-import android.support.animation.SpringForce
 import android.support.design.widget.Snackbar
-import android.util.Log
+import android.text.TextUtils
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -21,7 +17,7 @@ import com.google.android.flexbox.AlignContent
 import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayout
 import com.iflytek.cloud.*
-import com.iflytek.msc.MSC
+import com.iflytek.cloud.ui.RecognizerDialog
 import com.jaredrummler.android.processes.AndroidProcesses
 import com.kiven.kutils.activityHelper.KActivityHelper
 import com.kiven.kutils.activityHelper.KHelperActivity
@@ -29,9 +25,7 @@ import com.kiven.kutils.logHelper.KLog
 import com.kiven.kutils.tools.KAlertDialogHelper
 import com.kiven.kutils.tools.KGranting
 import com.kiven.sample.anim.AHAnim
-import com.kiven.sample.service.LiveWallpaper
 import com.kiven.sample.service.LiveWallpaper2
-import com.kiven.sample.util.WallpaperUtil
 
 /**
  * Created by wangk on 2018/3/28.
@@ -114,33 +108,17 @@ class AHSmallAction : KActivityHelper() {
         addTitle("语音识别")
 
         // http://doc.xfyun.cn/msc_android/%E9%A2%84%E5%A4%87%E5%B7%A5%E4%BD%9C.html
-        SpeechUtility.createUtility(mActivity, SpeechConstant.APPID + "=5a15147f")
-        val mAsr = SpeechRecognizer.createRecognizer(mActivity, {code ->
-            KLog.i("SpeechRecognizer init() code = $code")
-            if (code != ErrorCode.SUCCESS) {
-                showTip("初始化失败,错误码：$code")
-            }
-
-        })
-        // 设置引擎类型
-        //设置语法ID和 SUBJECT 为空，以免因之前有语法调用而设置了此参数；或直接清空所有参数，具体可参考 DEMO 的示例。
-        mAsr.setParameter( SpeechConstant.CLOUD_GRAMMAR, null )
-        mAsr.setParameter( SpeechConstant.SUBJECT, null )
-
-        mAsr.setParameter( SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD )
-        mAsr.setParameter(SpeechConstant.TEXT_ENCODING, "utf-8")
-
-
-        /* 其中 "abnf" 指定语法类型为 ABNF,  grammarContent 为语法内容，grammarListener 为构建结果监听器*/
-        /*val ret = mAsr.buildGrammar( "abnf", "", {a, b ->
-
-        } )*/
-
+        val mAsr = getXunfei()
         addView("讯飞识别", View.OnClickListener {
-            val ret = mAsr.startListening(mRecognizerListener)
-            if (ret == ErrorCode.SUCCESS) {
-                showTip("听写失败,错误码：$ret")
-            }
+            KGranting.requestPermissions(activity, 377, Manifest.permission.RECORD_AUDIO,
+                    "录音", KGranting.GrantingCallBack {
+                if (it) {
+                    val ret = mAsr.startListening(mRecognizerListener)
+                    if (ret != ErrorCode.SUCCESS) {
+                        showTip("听写失败,错误码：$ret")
+                    }
+                }
+            })
         })
 
         // TODO: 2018/3/28 ----------------------------------------------------------
@@ -153,15 +131,50 @@ class AHSmallAction : KActivityHelper() {
         addView("杀死一个进程杀死一个进程杀死一个进程", View.OnClickListener { })
     }
 
+    /**
+     * 初始讯飞
+     * http://doc.xfyun.cn/msc_android/%E9%A2%84%E5%A4%87%E5%B7%A5%E4%BD%9C.html
+     */
+    private fun getXunfei():SpeechRecognizer {
+        SpeechUtility.createUtility(mActivity, SpeechConstant.APPID + "=5a15147f")
+
+        val mAsr = SpeechRecognizer.createRecognizer(mActivity, { code ->
+            KLog.i("SpeechRecognizer init() code = $code")
+            if (code != ErrorCode.SUCCESS) {
+                showTip("初始化失败,错误码：$code")
+            }
+
+        })
+        // 设置引擎类型
+        //设置语法ID和 SUBJECT 为空，以免因之前有语法调用而设置了此参数；或直接清空所有参数，具体可参考 DEMO 的示例。
+        mAsr.setParameter(SpeechConstant.CLOUD_GRAMMAR, null)
+        mAsr.setParameter(SpeechConstant.SUBJECT, null)
+
+        mAsr.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD)
+        mAsr.setParameter(SpeechConstant.TEXT_ENCODING, "utf-8")
+
+        val dia = RecognizerDialog(mActivity, {code ->
+            KLog.i("SpeechRecognizer init() code = $code")
+            if (code != ErrorCode.SUCCESS) {
+                showTip("初始化失败,错误码：$code")
+            }
+        })
+        dia.show()
+
+        return mAsr
+    }
+
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         KGranting.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     private fun showTip(word: String) {
-        KAlertDialogHelper.Show1BDialog(mActivity, word)
+//        KAlertDialogHelper.Show1BDialog(mActivity, word)
+        KLog.d(word)
     }
 
+    val mTranslateEnable = false
     private val mRecognizerListener = object : RecognizerListener {
 
         override fun onBeginOfSpeech() {
@@ -198,11 +211,11 @@ class AHSmallAction : KActivityHelper() {
         }
 
         override fun onVolumeChanged(volume: Int, data: ByteArray) {
-            showTip("当前正在说话，音量大小：$volume")
+            KLog.i("当前正在说话，音量大小：$volume")
             KLog.i("返回音频数据：" + data.size)
         }
 
-        override fun onEvent(eventType: Int, arg1: Int, arg2: Int, obj: Bundle) {
+        override fun onEvent(eventType: Int, arg1: Int, arg2: Int, obj: Bundle?) {
             // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
             // 若使用本地能力，会话id为null
             //	if (SpeechEvent.EVENT_SESSION_ID == eventType) {
@@ -210,5 +223,13 @@ class AHSmallAction : KActivityHelper() {
             //		Log.d(TAG, "session id =" + sid);
             //	}
         }
+    }
+
+
+    private fun printTransResult(results: RecognizerResult) {
+        KLog.d("结果t：${results.resultString}")
+    }
+    private fun printResult(results: RecognizerResult) {
+        KLog.d("结果：${results.resultString}")
     }
 }

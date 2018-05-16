a55859a84e071fa8790480efde21f78e6e9ba8b4
kivenspc\wangkiven <984768569@qq.com>
2018-05-16 18:04:05 +0800

    opengl es demo 基本图形demo

--- sample/src/main/java/com/kiven/sample/gl/AHGL.kt
+++ sample/src/main/java/com/kiven/sample/gl/AHGL.kt
@@ -7,6 +7,8 @@ import android.opengl.GLU
 import android.os.Bundle
 import com.kiven.kutils.activityHelper.KActivityHelper
 import com.kiven.kutils.activityHelper.KHelperActivity
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -23,13 +25,26 @@ class AHGL : KActivityHelper() {
         setContentView(surfaceView)
     }
 
+    override fun onResume() {
+        super.onResume()
+        surfaceView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        surfaceView.onPause()
+    }
+
     class CameraGLSurfaceView(context: Context) : GLSurfaceView(context), GLSurfaceView.Renderer, SurfaceTexture.OnFrameAvailableListener {
         init {
-            // 设置OpenGl ES的版本为2.0，需在manifests配置gl版本
-            setEGLContextClientVersion(2)
+            // 设置OpenGl ES的版本为2.0，需在manifests配置gl版本。配置后，居然不能绘图
+//            setEGLContextClientVersion(2)
             // 设置与当前GLSurfaceView绑定的Renderer
             setRenderer(this)
-            // 设置渲染的模式
+
+            // 设置渲染的模式，
+            // RENDERMODE_WHEN_DIRTY：只有在调用 requestRender() 在更新屏幕，
+            // RENDERMODE_CONTINUOUSLY：连续不断的更新屏幕
             renderMode = RENDERMODE_WHEN_DIRTY
         }
 
@@ -72,10 +87,46 @@ class AHGL : KActivityHelper() {
             gl.glLoadIdentity()
         }
 
+        var vertexArray = floatArrayOf(
+                -0.8f, -0.4f * 1.732f, 0.0f,
+                0.8f, -0.4f * 1.732f, 0.0f,
+                0.0f, 0.4f * 1.732f, 0.0f)
+
         override fun onDrawFrame(gl: GL10) {
             gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f)
             // Clears the screen and depth buffer.
             gl.glClear(GL10.GL_COLOR_BUFFER_BIT or GL10.GL_DEPTH_BUFFER_BIT)
+
+            val vbb = ByteBuffer.allocateDirect(vertexArray.size * 4)
+            vbb.order(ByteOrder.nativeOrder())
+            val vertex = vbb.asFloatBuffer()
+            vertex.put(vertexArray)
+            vertex.position(0)
+
+            gl.glPointSize(20f)
+            gl.glLineWidth(4f)
+            gl.glLoadIdentity()
+            gl.glTranslatef(0f, 0f, -4f)
+
+            gl.glEnableClientState(GL10.GL_VERTEX_ARRAY)// 开启管道
+
+            gl.glColor4f(1.0f, 0.0f, 0.0f, 1.0f)
+            gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertex)
+            gl.glDrawArrays(GL10.GL_POINTS, 0, 3)
+//            gl.glDisableClientState(GL10.GL_VERTEX_ARRAY)
+
+
+            gl.glColor4f(0.0f, 1.0f, 0.0f, 1.0f)
+//            gl.glEnableClientState(GL10.GL_VERTEX_ARRAY)
+//            gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertex)
+            gl.glDrawArrays(GL10.GL_LINE_LOOP, 0, 3)
+
+
+            gl.glColor4f(0.0f, 0.0f, 1.0f, 0.1f)
+            gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3)
+
+
+            gl.glDisableClientState(GL10.GL_VERTEX_ARRAY)// 关闭管道
 
         }
     }

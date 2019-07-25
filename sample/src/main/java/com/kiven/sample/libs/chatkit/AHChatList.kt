9d8c3c4bce122dc0c298eda5beaebccf55fbff90
王小林 <984768569@qq.com>
2019-07-25 15:43:27 +0800

    *

--- sample/src/main/java/com/kiven/sample/libs/chatkit/AHChatList.kt
+++ sample/src/main/java/com/kiven/sample/libs/chatkit/AHChatList.kt
@@ -1,15 +1,127 @@
 package com.kiven.sample.libs.chatkit
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.kiven.kutils.activityHelper.KActivityDebugHelper
 import com.kiven.kutils.activityHelper.KHelperActivity
+import com.stfalcon.chatkit.commons.ImageLoader
+import com.stfalcon.chatkit.commons.models.IDialog
+import com.stfalcon.chatkit.commons.models.IMessage
+import com.stfalcon.chatkit.commons.models.IUser
+import com.stfalcon.chatkit.dialogs.DialogsList
+import com.stfalcon.chatkit.dialogs.DialogsListAdapter
+import org.jetbrains.anko.appcompat.v7.toolbar
+import org.jetbrains.anko.linearLayout
+import java.util.*
 
-class AHChatList:KActivityDebugHelper() {
+class AHChatList : KActivityDebugHelper() {
+
+    val adapter by lazy {
+        DialogsListAdapter<DefaultDailog>(ImageLoader { imageView, url, payload ->
+            imageView?.let { Glide.with(mActivity).load(url).circleCrop().into(it) }
+        })
+    }
+
+
     override fun onCreate(activity: KHelperActivity, savedInstanceState: Bundle?) {
         super.onCreate(activity, savedInstanceState)
-        val listView = RecyclerView(activity)
-        setContentView(listView)
+//        val listView = RecyclerView(activity)
+//        setContentView(listView)
 
+        val listView = DialogsList(activity)
+        listView.setAdapter(adapter)
+//        setContentView(listView)
+        activity.linearLayout {
+            orientation = LinearLayout.VERTICAL
+            toolbar {
+                initBackToolbar(this)
+            }
+            addView(listView as View, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menu.add(0, Menu.FIRST + 1, 0, "一条新对话")
+        menu.add(0, Menu.FIRST + 2, 0, "两条新对话")
+        menu.add(0, Menu.FIRST + 3, 1, "一条图片对话")
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            Menu.FIRST + 1 -> {
+                adapter.addItem(DefaultDailog())
+            }
+            Menu.FIRST + 2 -> {}
+            Menu.FIRST + 3 -> {}
+        }
+        return true
+    }
+
+    class DefaultDailog : IDialog<DefaultMessage> {
+        var lastMsg: DefaultMessage? = null
+        var dialogId = "0"
+
+        override fun getDialogPhoto(): String {
+            return "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564045468923&di=9316f56ad4bb4d9232658d249f94f11f&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fblog%2F201406%2F16%2F20140616171020_TiEXR.thumb.700_0.jpeg"
+        }
+
+        override fun getUnreadCount(): Int {
+            return (Math.random() * 100).toInt()
+        }
+
+        override fun setLastMessage(message: DefaultMessage?) {
+            lastMsg = message
+        }
+
+        override fun getId(): String {
+            return dialogId
+        }
+
+        override fun getUsers(): MutableList<out IUser> {
+            return mutableListOf()
+        }
+
+        override fun getLastMessage(): DefaultMessage? {
+            return lastMsg
+        }
+
+        override fun getDialogName(): String {
+            return "群名"
+        }
+
+    }
+
+    class DefaultMessage(private val msgId: String,
+                         private val time: Date,
+                         private val who: IUser,
+                         private val msgInfo: String) : IMessage {
+        override fun getId(): String = msgId
+
+        override fun getCreatedAt(): Date = time
+
+        override fun getUser(): IUser = who
+
+        override fun getText(): String = msgInfo
+
+    }
+
+    class DefaultUser(private val userId: String,
+                      private val userName: String,
+                      private val header: String) : IUser {
+
+        override fun getAvatar(): String = header
+
+        override fun getName(): String = userName
+
+        override fun getId(): String = userId
     }
 }

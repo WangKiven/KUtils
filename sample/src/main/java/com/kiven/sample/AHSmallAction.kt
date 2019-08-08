9d06924ea33d0f35c8630b3a3673b74356f3e677
王小林 <984768569@qq.com>
2019-08-08 16:26:43 +0800

    *

--- sample/src/main/java/com/kiven/sample/AHSmallAction.kt
+++ sample/src/main/java/com/kiven/sample/AHSmallAction.kt
@@ -3,14 +3,15 @@ package com.kiven.sample
 import android.Manifest
 import android.app.*
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricPrompt
+import android.hardware.fingerprint.FingerprintManager
 import android.media.MediaRecorder
 import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.os.Environment
-import android.os.Process
+import android.os.*
 import android.provider.Settings
 import android.telephony.PhoneStateListener
 import android.telephony.TelephonyManager
@@ -19,6 +20,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.hardware.fingerprint.FingerprintManagerCompat
 import com.google.android.flexbox.AlignContent
 import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayout
@@ -482,7 +484,135 @@ class AHSmallAction : KActivityDebugHelper() {
                 Process.killProcess(Process.myPid())
             }
         })
-        addView("", View.OnClickListener { })
+        addView("指纹(生物)识别", View.OnClickListener {
+            // 需要配置权限：
+            // <!--指纹识别，低版本需要-->
+            // <uses-permission android:name="android.permission.USE_FINGERPRINT"/>
+            // <!-- 生物识别，高版本需要 -->
+            // <uses-permission android:name="android.permission.USE_BIOMETRIC"/>
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val bm = mActivity.getSystemService(BiometricManager::class.java)
+                showTip("api 29 检测生物识别功能")
+                when (bm.canAuthenticate()) {
+                    BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                        showTip("用户没设置生物识别。功能存在，但是用户没打开")
+                    }
+                    BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                        showTip("当前设备不支持或不可用生物识别")
+                    }
+                    BiometricManager.BIOMETRIC_SUCCESS -> {
+                        showTip("可以使用生物特征识别（已注册并可用）")
+                    }
+                    BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                        showTip("没有生物识别硬件")
+                    }
+                }
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val fm = mActivity.getSystemService(FingerprintManager::class.java)
+                // 确定指纹硬件是否存在且功能正常
+                if (!fm.isHardwareDetected) {
+                    showTip("指纹硬件不存在或不可用")
+                    return@OnClickListener
+                }
+                // 确定是否至少注册了一个指纹
+                if (!fm.hasEnrolledFingerprints()) {
+                    showTip("用户没设置指纹")
+                    return@OnClickListener
+                }
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                // 好像不需要在这里请求权限，在maiifest设置就够了
+                KGranting.requestPermissions(mActivity, 234, Manifest.permission.USE_BIOMETRIC, "指纹") {
+                    if (it) {
+                        val bp = BiometricPrompt.Builder(mActivity)
+                                .setTitle("指生物别测试")
+                                .setDescription("是什么流程呀？")
+                                .setNegativeButton("取消", mActivity.mainExecutor, DialogInterface.OnClickListener { _, i ->
+                                    showTip("点击取消了 $i")
+                                })
+                                .build()
+
+                        val cs = CancellationSignal()
+                        cs.setOnCancelListener { showTip("CancellationSignal 取消什么") }
+
+                        val ac = object : BiometricPrompt.AuthenticationCallback() {
+                            override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                                super.onAuthenticationError(errorCode, errString)
+                                showTip("AuthenticationCallback onAuthenticationError 设备没有开启生物识别")
+                            }
+
+                            override fun onAuthenticationFailed() {
+                                super.onAuthenticationFailed()
+                                showTip("AuthenticationCallback onAuthenticationFailed 识别失败")
+                            }
+
+                            override fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?) {
+                                super.onAuthenticationHelp(helpCode, helpString)
+                                showTip("AuthenticationCallback onAuthenticationHelp")
+                            }
+
+                            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                                super.onAuthenticationSucceeded(result)
+                                showTip("AuthenticationCallback onAuthenticationSucceeded 识别成功")
+                            }
+                        }
+
+                        bp.authenticate(cs, mActivity.mainExecutor, ac)
+//                        cs.cancel()
+                    }
+                }
+
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    val fm = mActivity.getSystemService(FingerprintManager::class.java)
+                    val cs = CancellationSignal()
+                    cs.setOnCancelListener { showTip("CancellationSignal 取消什么") }
+
+                    val tv = TextView(mActivity)
+                    tv.text = "放上手指开始识别"
+                    val dialog = AlertDialog.Builder(mActivity)
+                            .setView(tv)
+                            .setOnCancelListener {
+                                cs.cancel()
+                            }
+                            .create()
+
+                    val ac = object : FingerprintManager.AuthenticationCallback() {
+                        override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                            super.onAuthenticationError(errorCode, errString)
+                            // CancellationSignal.cancel() 也会到这里来
+                            showTip("AuthenticationCallback onAuthenticationError $errorCode-$errString")
+                            dialog.hide()
+                        }
+
+                        override fun onAuthenticationFailed() {
+                            super.onAuthenticationFailed()
+                            showTip("AuthenticationCallback onAuthenticationFailed")
+                            tv.text = "验证失败"
+                        }
+
+                        override fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?) {
+                            super.onAuthenticationHelp(helpCode, helpString)
+                            showTip("AuthenticationCallback onAuthenticationHelp")
+                        }
+
+                        override fun onAuthenticationSucceeded(result: FingerprintManager.AuthenticationResult?) {
+                            super.onAuthenticationSucceeded(result)
+                            showTip("AuthenticationCallback onAuthenticationSucceeded")
+                            dialog.hide()
+                        }
+                    }
+
+                    dialog.show()
+                    fm.authenticate(null, cs, 0, ac, null)
+//                    cs.cancel()
+                }
+            }
+        })
         addView("", View.OnClickListener { })
         addView("", View.OnClickListener { })
         addView("", View.OnClickListener { })

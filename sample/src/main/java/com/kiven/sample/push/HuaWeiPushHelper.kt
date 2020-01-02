633b486d26112039711a1d1067f869f8b7a0842d
王小林 <984768569@qq.com>
2020-01-02 15:39:59 +0800

    *

--- sample/src/main/java/com/kiven/sample/push/HuaWeiPushHelper.kt
+++ sample/src/main/java/com/kiven/sample/push/HuaWeiPushHelper.kt
@@ -11,12 +11,21 @@ object HuaWeiPushHelper {
     var token:String? = null
     /**
      * 可能耗时，需异步
+     *
+     * 出现异常的可能情况：
+     * 1 下载的 agconnect-services.json 文件名称不对，有可能下载下来文件名称是"agconnect-services.json.txt"，需要去掉".txt"
+     *
+     *
+     * 华为测试机token: 0865265045829291300005487100CN01
      */
     fun initHuaWeiPush(context: Context):Boolean {
         return try {
             val appId = AGConnectServicesConfig.fromContext(context).getString("client/app_id")
+            KLog.i("华为appId: $appId")
+
+            // TODO Token发生变化时或者EMUI版本低于10.0以 onNewToken 方法返回
             token = HmsInstanceId.getInstance(context).getToken(appId, "HCM")
-            KLog.i("华为token: $token")
+            KLog.i("HmsInstanceId获取华为token: $token")
 
             true
         } catch (e: Exception) {
--- sample/src/main/java/com/kiven/sample/service/HuaWeiPushService.kt
+++ sample/src/main/java/com/kiven/sample/service/HuaWeiPushService.kt
@@ -3,11 +3,16 @@ package com.kiven.sample.service
 import com.huawei.hms.push.HmsMessageService
 import com.huawei.hms.push.RemoteMessage
 import com.kiven.kutils.logHelper.KLog
+import com.kiven.sample.push.HuaWeiPushHelper
 
 /**
  * Created by oukobayashi on 2019-12-31.
  */
 class HuaWeiPushService:HmsMessageService() {
+    /**
+     * 目前测试结果：
+     * 仅接收透传消息，并且在App已经启动的情况下才能收到
+     */
     override fun onMessageReceived(p0: RemoteMessage?) {
         super.onMessageReceived(p0)
         KLog.i("onMessageReceived ${p0?.data}")
@@ -16,5 +21,11 @@ class HuaWeiPushService:HmsMessageService() {
     override fun onMessageSent(p0: String?) {
         super.onMessageSent(p0)
         KLog.i("onMessageSent $p0")
+    }
+
+    override fun onNewToken(p0: String?) {
+        super.onNewToken(p0)
+        HuaWeiPushHelper.token = p0
+        KLog.i("HmsMessageService接收的华为token: $p0")
     }
 }

0662f4ac70046c2e8dba416dd4ef6e1ea0be417c
王小林 <984768569@qq.com>
2020-02-25 17:41:05 +0800

    *

--- sample/src/main/java/com/kiven/sample/noti/MyNotificationListenerService.kt
+++ sample/src/main/java/com/kiven/sample/noti/MyNotificationListenerService.kt
@@ -3,18 +3,59 @@ package com.kiven.sample.noti
 import android.os.Build
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.speech.tts.TextToSpeech
 import androidx.annotation.RequiresApi
+import com.kiven.kutils.logHelper.KLog
 import com.kiven.sample.util.showTip
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 /**
  * Created by oukobayashi on 2019-12-11.
  * https://blog.csdn.net/xiayiye5/article/details/83688396
  */
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-class MyNotificationListenerService: NotificationListenerService() {
+class MyNotificationListenerService : NotificationListenerService() {
+    var text2speech: TextToSpeech? = null
+
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         sbn?.apply {
-            showTip("监听到通知：${notification.tickerText}")
+            val text = notification.tickerText
+
+            showTip("监听到通知：${text}")
+
+            // 不判空华为崩溃，小米没有崩溃
+            if (text?.isNullOrBlank() == true) {
+                return
+            }
+            GlobalScope.launch {
+                if (text2speech == null){
+                    val result = suspendCoroutine<Boolean> {
+                        text2speech = TextToSpeech(this@MyNotificationListenerService, TextToSpeech.OnInitListener { status ->
+                            if (status == TextToSpeech.SUCCESS) {
+                                it.resume(true)
+                                return@OnInitListener
+                            }
+                            it.resume(false)
+                        })
+                    }
+
+                    if (result) {
+                        text2speech?.apply {
+                            val supported = setLanguage(Locale.SIMPLIFIED_CHINESE)
+                            if (supported != TextToSpeech.LANG_AVAILABLE && supported != TextToSpeech.LANG_COUNTRY_AVAILABLE) {
+                                KLog.i("不支持当前语言！")
+                            }
+                        }
+                    }
+                }
+
+                // QUEUE_ADD 添加新的，QUEUE_FLUSH 刷新(即清除之前的，会打断正在播放的)
+                text2speech?.speak(text.toString(), TextToSpeech.QUEUE_FLUSH, null)
+            }
         }
     }
 

9fdc809b0d99011734b9e34897f3925ec6d8fdea
王小林 <984768569@qq.com>
2020-04-02 15:48:17 +0800

    *

--- pushlibrary/src/main/java/com/kiven/pushlibrary/PushUtil.kt
+++ pushlibrary/src/main/java/com/kiven/pushlibrary/PushUtil.kt
@@ -7,12 +7,9 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
-import android.media.RingtoneManager
 import android.os.Build
-import android.os.Bundle
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import com.kiven.kutils.logHelper.KLog
 
 object PushUtil {
 
@@ -21,24 +18,35 @@ object PushUtil {
      * 这个不要动，华为推送，服务器用了这个channelId
      */
 //    val channelId = "sxbChannelId"
-    fun getChannelId(context: Context):String {
-        when(Web.platform) {//设备类型 0 不明，1 iOS, 2 华为, 3 vivo, 4 oppo, 5 小米
-            2 -> {}
-            3 -> return "vivo_push_channel"
-            4 -> {}
+    fun getChannelId(context: Context): String {
+        when (Web.platform) {//设备类型 0 不明，1 iOS, 2 华为, 3 vivo, 4 oppo, 5 小米
+            2 -> {
+            }
+            3 -> {
+                // TODO vivo 远程推送的channel,本地也可以使用。远程推送时创建的channel默认是开启的, 本地创建的默认是关闭的。
+                //  所以等待远程推送并创建好channel后再，使用webSocket推送
+                if (Build.VERSION.SDK_INT >= 26) return "vivo_push_channel"
+            }
+            4 -> {
+            }
         }
         return "sxbChannelId"
     }
-    val channelName = "推送通知"
 
-    fun initChannel(context: Context){
+    private val channelName = "推送通知"
+
+    fun initChannel(context: Context) {
         val notiManager = NotificationManagerCompat.from(context)
         val channelId = getChannelId(context)
-KLog.i("创建$channelName")
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channels = notiManager.notificationChannels
             if (channels.filter { it.id == channelId }.isNullOrEmpty()) {
-                val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+                val channel = NotificationChannel(
+                    channelId,
+                    channelName,
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
                 channel.enableLights(true)
                 channel.lightColor = Color.GREEN
                 /*if (p < groups.size) {
@@ -48,7 +56,8 @@ KLog.i("创建$channelName")
                 channel.enableVibration(true) // 震动
                 //channel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC // 锁屏可见
                 channel.setShowBadge(true)
-                channel.importance = NotificationManager.IMPORTANCE_HIGH//重要性，不重要的通知可能没声音，也可能被收纳起来导致用户看不到
+                channel.importance =
+                    NotificationManager.IMPORTANCE_HIGH//重要性，不重要的通知可能没声音，也可能被收纳起来导致用户看不到
                 channel.description = "推送通知" // 描述
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
@@ -73,18 +82,19 @@ KLog.i("创建$channelName")
 //        val ii = Intent(context, Class.forName("com.kiven.sample.noti.ClickNotiActivity"))
         val ii = Intent(context, Class.forName("com.kiven.pushlibrary.ClickNotiActivity"))
         ii.putExtra("argu", argument)
-        val pendingIntent = PendingIntent.getActivity(context, 110, ii, PendingIntent.FLAG_UPDATE_CURRENT)
+        val pendingIntent =
+            PendingIntent.getActivity(context, 110, ii, PendingIntent.FLAG_UPDATE_CURRENT)
 
         val mBuilder = NotificationCompat.Builder(context, getChannelId(context))
-                .setSmallIcon(context.applicationContext.applicationInfo.icon)
+            .setSmallIcon(context.applicationContext.applicationInfo.icon)
 //                .setTicker(subTitle) // 通知响起时，状态栏显示的内容
-                .setContentTitle(title)
-                .setContentText(subTitle)
+            .setContentTitle(title)
+            .setContentText(subTitle)
 //                .setNumber(12)
 //                .setContentInfo(subTitle)//在通知的右侧设置大文本。
-                .setAutoCancel(true)
-                .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL)//图标类型
-                .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL)//图标类型
+            .setContentIntent(pendingIntent)
 
         // Notification#DEFAULT_SOUND Notification#DEFAULT_VIBRATE
         // Notification#DEFAULT_LIGHTS Notification#DEFAULT_ALL

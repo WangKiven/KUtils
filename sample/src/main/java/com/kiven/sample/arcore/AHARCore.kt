af37fdce8db1480295d1664eee2a29f52f569473
王小林 <984768569@qq.com>
2020-04-25 23:21:03 +0800

    *

--- sample/src/main/java/com/kiven/sample/arcore/AHARCore.kt
+++ sample/src/main/java/com/kiven/sample/arcore/AHARCore.kt
@@ -12,9 +12,11 @@ import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayout
 import com.google.ar.core.ArCoreApk
 import com.google.ar.core.Session
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
 import com.kiven.kutils.activityHelper.KActivityDebugHelper
 import com.kiven.kutils.activityHelper.KHelperActivity
 import com.kiven.kutils.tools.KGranting
+import com.kiven.sample.util.snackbar
 import org.jetbrains.anko.support.v4.nestedScrollView
 
 /**
@@ -54,18 +56,30 @@ class AHARCore : KActivityDebugHelper() {
         supportARCoreTextView = addTitle("ARCore检测中")
         checkARCore()
 
+        var session:Session? = null
         addView("go", View.OnClickListener {
+            if (session != null) return@OnClickListener
+
             KGranting.requestPermissions(activity, 233, Manifest.permission.CAMERA) {
                 if (it) {
-                    when(ArCoreApk.getInstance().requestInstall(activity, true)) {
-                        ArCoreApk.InstallStatus.INSTALLED -> {
-                            val session = Session(activity)
+                    try {
+                        when(ArCoreApk.getInstance().requestInstall(activity, true)) {
+                            ArCoreApk.InstallStatus.INSTALLED -> {
+                                session = Session(activity)
+
+                            }
+                            ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
+                                //如果 requestInstall() 返回 INSTALL_REQUESTED，则当前 Activity 将暂停，并提示用户安装或更新 ARCore：
+                            }
+                            else -> {}
                         }
-                        ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
-                            //如果 requestInstall() 返回 INSTALL_REQUESTED，则当前 Activity 将暂停，并提示用户安装或更新 ARCore：
-                        }
-                        else -> {}
+                    }catch (e: UnavailableUserDeclinedInstallationException) {
+                        activity.snackbar("不可用异常")
+                    }catch (e:Throwable) {
+                        activity.snackbar("不明异常 ：${e.message}")
+                        e.printStackTrace()
                     }
+
                 }
             }
         })

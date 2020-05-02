55cd2ebae551ff4c122ef5536e9c1d948de9c355
王小林 <984768569@qq.com>
2020-05-03 01:27:57 +0800

    *

--- sample/src/main/java/com/kiven/sample/arcore/AHARCore.kt
+++ sample/src/main/java/com/kiven/sample/arcore/AHARCore.kt
@@ -13,10 +13,17 @@ import com.google.android.flexbox.FlexboxLayout
 import com.google.ar.core.ArCoreApk
 import com.google.ar.core.Session
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.Color
+import com.google.ar.sceneform.rendering.MaterialFactory
+import com.google.ar.sceneform.rendering.ShapeFactory
+import com.google.ar.sceneform.ux.ArFragment
 import com.kiven.kutils.activityHelper.KActivityDebugHelper
 import com.kiven.kutils.activityHelper.KHelperActivity
 import com.kiven.kutils.tools.KGranting
+import com.kiven.sample.R
 import com.kiven.sample.util.snackbar
+import org.jetbrains.anko.frameLayout
 import org.jetbrains.anko.support.v4.nestedScrollView
 
 /**
@@ -26,13 +33,23 @@ class AHARCore : KActivityDebugHelper() {
     var supportARCoreTextView: TextView? = null
     var isSupport = -1
 
+    val arFragment = ArFragment()
     override fun onCreate(activity: KHelperActivity, savedInstanceState: Bundle?) {
         super.onCreate(activity, savedInstanceState)
+
         val flexboxLayout = FlexboxLayout(activity)
         flexboxLayout.flexWrap = FlexWrap.WRAP
         flexboxLayout.alignContent = AlignContent.FLEX_START
 
-        mActivity.nestedScrollView { addView(flexboxLayout) }
+        activity.frameLayout {
+            id = R.id.ll_root
+
+            activity.supportFragmentManager.beginTransaction().add(R.id.ll_root, arFragment, null).commit()
+
+            nestedScrollView {
+                addView(flexboxLayout)
+            }
+        }
 
         val addTitle = fun(text: String): TextView {
             val tv = TextView(activity)
@@ -67,6 +84,10 @@ class AHARCore : KActivityDebugHelper() {
                             ArCoreApk.InstallStatus.INSTALLED -> {
                                 session = Session(activity)
 
+                                /*MaterialFactory.makeOpaqueWithColor(activity, Color(android.graphics.Color.RED))
+                                        .thenAccept {
+                                            val redSphereRenderable = ShapeFactory.makeSphere(0.1f, Vector3(0.0f, 0.15f, 0.0f), it)
+                                        }*/
                             }
                             ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
                                 //如果 requestInstall() 返回 INSTALL_REQUESTED，则当前 Activity 将暂停，并提示用户安装或更新 ARCore：
@@ -83,6 +104,8 @@ class AHARCore : KActivityDebugHelper() {
                 }
             }
         })
+
+
     }
 
     private fun checkARCore() {

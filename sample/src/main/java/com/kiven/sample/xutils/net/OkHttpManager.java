e62cefae02b7090a971cb28c5006adfd79b1c394
王小林 <984768569@qq.com>
2020-06-28 16:44:00 +0800

    *

--- sample/src/main/java/com/kiven/sample/xutils/net/OkHttpManager.java
+++ sample/src/main/java/com/kiven/sample/xutils/net/OkHttpManager.java
@@ -2,9 +2,11 @@ package com.kiven.sample.xutils.net;
 
 import org.xutils.HttpManager;
 import org.xutils.common.Callback;
+import org.xutils.http.HttpManagerImpl;
 import org.xutils.http.HttpMethod;
 import org.xutils.http.RequestParams;
 
+import java.lang.reflect.Type;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
@@ -43,21 +45,56 @@ public class OkHttpManager implements HttpManager {
 
     @Override
     public <T> T getSync(RequestParams entity, Class<T> resultType) throws Throwable {
-        return null;
+        return requestSync(HttpMethod.GET, entity, resultType);
     }
 
     @Override
     public <T> T postSync(RequestParams entity, Class<T> resultType) throws Throwable {
-        return null;
+        return requestSync(HttpMethod.POST, entity, resultType);
     }
 
     @Override
     public <T> T requestSync(HttpMethod method, RequestParams entity, Class<T> resultType) throws Throwable {
-        return null;
+        DefaultSyncCallback<T> callback = new DefaultSyncCallback<T>(resultType);
+        return requestSync(method, entity, callback);
     }
 
     @Override
     public <T> T requestSync(HttpMethod method, RequestParams entity, Callback.TypedCallback<T> callback) throws Throwable {
         return null;
+    }
+
+    private static class DefaultSyncCallback<T> implements Callback.TypedCallback<T> {
+
+        private final Class<T> resultType;
+
+        public DefaultSyncCallback(Class<T> resultType) {
+            this.resultType = resultType;
+        }
+
+        @Override
+        public Type getLoadType() {
+            return resultType;
+        }
+
+        @Override
+        public void onSuccess(T result) {
+
+        }
+
+        @Override
+        public void onError(Throwable ex, boolean isOnCallback) {
+
+        }
+
+        @Override
+        public void onCancelled(CancelledException cex) {
+
+        }
+
+        @Override
+        public void onFinished() {
+
+        }
     }
 }

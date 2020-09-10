70ee2599eb8544f2d0840b339e80006167294d9e
王小林 <984768569@qq.com>
2020-09-10 19:34:16 +0800

    GlobalScope添加主线程调用方法

--- kutils-ktx/src/main/java/com/sxb/kutils_ktx/util/KKUtil.kt
+++ kutils-ktx/src/main/java/com/sxb/kutils_ktx/util/KKUtil.kt
@@ -2,6 +2,7 @@ package com.sxb.kutils_ktx.util
 
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.coroutines.*
 
 /**
  * 根据 Parcel 生成 Parcelable 对象的实例。由于 Kotlin 的问题，导致 @Parcelize注解的Parcelable子类 不能直接调用 CREATOR ，只能通过反射来了。
@@ -11,3 +12,8 @@ fun <T : Parcelable> createFromParcel(t: Class<T>, parcel: Parcel): T {
     val creator = field.get(t) as Parcelable.Creator<T>
     return creator.createFromParcel(parcel)
 }
+
+/**
+ * GlobalScope主线程调用
+ */
+fun GlobalScope.main(block: suspend CoroutineScope.() -> Unit): Job = GlobalScope.launch(Dispatchers.Main, block = block)

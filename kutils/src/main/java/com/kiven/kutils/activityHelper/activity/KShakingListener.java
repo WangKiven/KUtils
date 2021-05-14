e1289180b309cdcc01c3c1df8a93955d10993fc9
kivenC <984768569>
2021-05-14 16:59:52 +0800

    陀螺仪速度计算优化

--- kutils/src/main/java/com/kiven/kutils/activityHelper/activity/KShakingListener.java
+++ kutils/src/main/java/com/kiven/kutils/activityHelper/activity/KShakingListener.java
@@ -28,11 +28,12 @@ public class KShakingListener implements SensorEventListener {
             float y = event.values[1];
             float z = event.values[2];
 
-            float xx = Math.abs(x);
+            /*float xx = Math.abs(x);
             float yy = Math.abs(y);
             float zz = Math.abs(z);
 
-            double sens = Math.sqrt(xx * xx + yy * yy + zz * zz);
+            double sens = Math.sqrt(xx * xx + yy * yy + zz * zz);*/
+            double sens = Math.sqrt(x * x + y * y + z * z);//用于平方的数，不需要求绝对值
 
             if (sens > 25) {
                 onShaking.callBack();

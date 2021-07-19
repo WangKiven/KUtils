0b154f24c73e1eb770c45cb970a2ff25dd47c66c
kiven <984768569@qq.com>
2021-07-19 16:56:57 +0800

    库升级

--- pushlibrary/src/main/java/com/kiven/pushlibrary/firebase/FirebaseHelper.kt
+++ pushlibrary/src/main/java/com/kiven/pushlibrary/firebase/FirebaseHelper.kt
@@ -1,8 +1,8 @@
 package com.kiven.pushlibrary.firebase
 
 import android.content.Context
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.iid.FirebaseInstanceId
+//import com.google.android.gms.tasks.OnCompleteListener
+//import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessaging
 import com.kiven.kutils.logHelper.KLog
 import com.kiven.pushlibrary.PushHelper
@@ -20,7 +20,7 @@ class FirebaseHelper : PushHelper {
         // 此值一经设置便会持久保存，不受应用重启的影响
 //        FirebaseMessaging.getInstance().isAutoInitEnabled = true
 
-        FirebaseInstanceId.getInstance().instanceId
+        /*FirebaseInstanceId.getInstance().instanceId
                 .addOnCompleteListener(OnCompleteListener { task ->
                     if (!task.isSuccessful) {
                         KLog.e(task.exception)
@@ -32,7 +32,20 @@ class FirebaseHelper : PushHelper {
 
                     Web.register(context, token, 6)
                     KLog.i("firebaseToken = $token")
-                })
+                })*/
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                KLog.e(task.exception)
+                return@addOnCompleteListener
+            }
+
+            // Get new Instance ID token
+            val token = task.result ?: ""
+
+            Web.register(context, token, 6)
+            KLog.i("firebaseToken = $token")
+
+        }
     }
 
     // 没有找到获取已订阅的接口

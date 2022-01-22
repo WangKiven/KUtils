252b987ad8901e0250a3f0ec3c350883d4f20043
kiven <984768569@qq.com>
2022-01-22 17:07:47 +0800

    *

--- sample/src/main/java/com/kiven/sample/charCode/AHCharCode.kt
+++ sample/src/main/java/com/kiven/sample/charCode/AHCharCode.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import android.view.Gravity
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.unit.dp
 import androidx.core.widget.NestedScrollView
 import com.kiven.kutils.activityHelper.KActivityHelper
 import com.kiven.kutils.activityHelper.KHelperActivity
@@ -11,7 +17,6 @@ import com.kiven.kutils.logHelper.KLog
 import com.kiven.kutils.tools.KString
 import com.kiven.sample.util.getInput
 import com.kiven.sample.util.showListDialog
-import org.jetbrains.anko.*
 import java.nio.charset.Charset
 
 /**
@@ -54,58 +59,81 @@ class AHCharCode : KActivityHelper() {
     override fun onCreate(activity: KHelperActivity, savedInstanceState: Bundle?) {
         super.onCreate(activity, savedInstanceState)
 
-        setContentView(NestedScrollView(activity).apply {
+        activity.setContent {
+//            PaddingValues(30.dp)
+
+            Column {
+                Button(onClick = {
+                    mActivity.getInput("16进制", textCode) {
+                        textView?.text = StringCodeUtil.hexStr2Str(it.toString(), useCharset)
+                        textCode = it.toString()
+                    }
+                }) {
+                    Text(text = "16进制 转 字符")
+                }
+
+                Button(onClick = {
+                    mActivity.getInput("汉字", textChinese) {
+                        textView?.text = StringCodeUtil.str2HexStr(it.toString(), useCharset)
+                        textChinese = it.toString()
+
+                        // 后台输出更多
+                        charsets.forEach { cs ->
+                            KLog.i("$cs : ${StringCodeUtil.str2HexStr(it.toString(), cs)}")
+                        }
+                    }
+                }) {
+                    Text(text = "字符 转 16进制")
+                }
+
+                val cs = Charset.availableCharsets().keys.toList()
+                Button(onClick = {
+                    mActivity.showListDialog(
+                        cs.mapIndexed { i, c -> "$i - $c" }
+                    ) { index, _ ->
+                        useCharset = Charset.forName(cs[index])
+                    }
+                }) {
+                    Text(text = "选择编码 - 所有可用编码")
+                }
+
+                Button(onClick = {
+                    mActivity.showListDialog(
+                        charsets.map { it.toString() }
+                    ) { index, _ ->
+                        useCharset = charsets[index]
+                    }
+                }) {
+                    Text(text = "选择编码 - 仅列出常用编码")
+                }
+
+                Text(text = "点击按钮显示结果")
+
+
+                Text(
+                    text = "平面介绍：" +
+                            "\n- 常用的平面为0号平面（U+0000 - U+FFFF, 即基本多文种平面）" +
+                            "\n- 1号平面(U+10000 - U+1FFFF): 多文种补充平面" +
+                            "\n- 2号平面(U+20000 - U+2FFFF): 表意文字补充平面" +
+                            "\n- 3号平面(U+30000 - U+3FFFF): 表意文字第三平面（未正式使用）" +
+                            "\n- 4～13号平面(U+40000 - U+DFFFF): （尚未使用）" +
+                            "\n- 14号平面(U+E0000 - U+EFFFF): 特别用途补充平面" +
+                            "\n- 15号平面(U+F0000 - U+FFFFF): 保留作为私人使用区（A区）" +
+                            "\n- 16号平面(U+100000 - U+10FFFF): 保留作为私人使用区（B区）" +
+                            "\n组合字符：" +
+                            "\n- 组合用附加符号（Combining Diacritical Marks，0300–036F）" +
+                            "\n- 组合用附加符号扩展集（Combining Diacritical Marks Extended，1AB0–1AFF）" +
+                            "\n- 组合用附加符号增补集（Combining Diacritical Marks Supplement，1DC0–1DFF）" +
+                            "\n- 符号之组合用附加符号（Combining Diacritical Marks for Symbols，20D0–20FF）" +
+                            "\n- 组合用半形符号（Combining Half Marks，FE20–FE2F）"
+                )
+            }
+        }
+
+        /*setContentView(NestedScrollView(activity).apply {
             addView(LinearLayout(activity).apply {
                 orientation = LinearLayout.VERTICAL
                 topPadding = dip(30)
-
-                button {
-                    text = "16进制 转 字符"
-                    setOnClickListener {
-                        mActivity.getInput("16进制", textCode) {
-                            textView?.text = StringCodeUtil.hexStr2Str(it.toString(), useCharset)
-                            textCode = it.toString()
-                        }
-                    }
-                }
-                button {
-                    text = "字符 转 16进制"
-                    setOnClickListener {
-                        mActivity.getInput("汉字", textChinese) {
-                            textView?.text = StringCodeUtil.str2HexStr(it.toString(), useCharset)
-                            textChinese = it.toString()
-
-                            // 后台输出更多
-                            charsets.forEach { cs ->
-                                KLog.i("$cs : ${StringCodeUtil.str2HexStr(it.toString(), cs)}")
-                            }
-                        }
-                    }
-                }
-
-                button {
-                    text = "选择编码 - 所有可用编码"
-
-                    val cs = Charset.availableCharsets().keys.toList()
-                    setOnClickListener {
-                        mActivity.showListDialog(
-                                cs.mapIndexed { i, c -> "$i - $c" }
-                        ) { index, _ ->
-                            useCharset = Charset.forName(cs[index])
-                        }
-                    }
-                }
-
-                button {
-                    text = "选择编码 - 仅列出常用编码"
-                    setOnClickListener {
-                        mActivity.showListDialog(
-                                charsets.map { it.toString() }
-                        ) { index, _ ->
-                            useCharset = charsets[index]
-                        }
-                    }
-                }
 
                 textView {
                     textView = this
@@ -117,16 +145,6 @@ class AHCharCode : KActivityHelper() {
                         KString.setClipText(mActivity, text.toString())
                     }
                 }
-
-                /*button {
-                    text = "unicode 列表"
-                    setOnClickListener { AHUnicodeList().startActivity(activity) }
-                }*/
-
-                /*button {
-                    text = "unicode(1~16号平面) 列表"
-                    setOnClickListener { AHUnicodeList().startActivity(activity) }
-                }*/
 
                 textView {
                     text = "平面介绍：" +
@@ -146,7 +164,7 @@ class AHCharCode : KActivityHelper() {
                             "\n- 组合用半形符号（Combining Half Marks，FE20–FE2F）"
                 }
             })
-        })
+        })*/
 
     }
 }

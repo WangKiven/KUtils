2f08f15d04209b38091419c3d045fdc6e54b9574
kiven <984768569@qq.com>
2022-03-15 17:57:06 +0800

    *

--- sample/src/main/java/com/kiven/sample/compose/AHComposeDemo.kt
+++ sample/src/main/java/com/kiven/sample/compose/AHComposeDemo.kt
@@ -3,15 +3,19 @@ package com.kiven.sample.compose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kiven.kutils.activityHelper.KActivityHelper
@@ -20,7 +24,7 @@ import com.kiven.sample.R
 import com.kiven.sample.compose.theme.TestComposeTheme
 import kotlinx.coroutines.launch
 
-class AHComposeDemo: KActivityHelper() {
+class AHComposeDemo : KActivityHelper() {
     override fun onCreate(activity: KHelperActivity, savedInstanceState: Bundle?) {
         super.onCreate(activity, savedInstanceState)
 
@@ -34,21 +38,53 @@ class AHComposeDemo: KActivityHelper() {
 fun TestContent(scaffoldState: ScaffoldState = rememberScaffoldState()) {
     val scope = rememberCoroutineScope()
 
+
+    // todo https://developer.android.google.cn/jetpack/compose/layouts/material
     TestComposeTheme {
-        Scaffold(scaffoldState = scaffoldState,topBar = {
-            TopAppBar() {
+        Scaffold(scaffoldState = scaffoldState, topBar = {
+            TopAppBar {
+                Text(text = "菜单", Modifier.weight(1f))
+                Text(text = "title", Modifier.weight(1f), textAlign = TextAlign.Center)
+                Text(text = "设置", Modifier.weight(1f), textAlign = TextAlign.End)
             }
-        }) {
-            Column(modifier = Modifier
-                .padding(16.dp)
-                .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-                Greeting("Android")
+        }, bottomBar = {
+            BottomAppBar(cutoutShape = MaterialTheme.shapes.small.copy(CornerSize(percent = 50))) {
+
+            }
+        }, floatingActionButton = {
+//            FloatingActionButton(onClick = { /*TODO*/ }) {
+//                Icon(Icons.Filled.Add, contentDescription = "add")
+//            }
+            ExtendedFloatingActionButton(
+                onClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.apply {
+                            if (isClosed) open() else close()
+                        }
+                    }
+                },
+                icon = {
+                    Icon(
+                        Icons.Filled.Favorite,
+                        contentDescription = "Favorite"
+                    )
+                },
+                text = { Text("Like") }
+            )
+        }, isFloatingActionButtonDocked = true,
+        drawerContent = {}, drawerGesturesEnabled = true) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Greeting("Android1")
                 Greeting("Android2")
 
                 var count by remember {
                     mutableStateOf(0)
                 }
-                Button(onClick = { count ++ }) {
+                Button(onClick = { count++ }) {
                     Text(text = "click ${count}!")
                 }
 
@@ -57,16 +93,25 @@ fun TestContent(scaffoldState: ScaffoldState = rememberScaffoldState()) {
                     mutableStateOf(0)
                 }
                 Button(onClick = {
-                    count2 ++
+                    count2++
 
                     scope.launch {
                         scaffoldState.snackbarHostState.showSnackbar("click ${count2}!")
                     }
                 }) {
+                    Icon(
+                        Icons.Filled.Favorite,
+                        contentDescription = "Favorite",
+                        modifier = Modifier.size(ButtonDefaults.IconSize)
+                    )
+                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
                     Text(text = "click ${count2}!")
                 }
 
-                Image(painter = painterResource(id = R.mipmap.ic_launcher_u), contentDescription = "")
+                Image(
+                    painter = painterResource(id = R.mipmap.ic_launcher_u),
+                    contentDescription = ""
+                )
             }
         }
 

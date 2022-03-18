7e2a0f088dd2118282572a75ff917e854a1a1b5a
kiven <984768569@qq.com>
2022-03-18 17:48:43 +0800

    *

--- sample/src/main/java/com/kiven/sample/compose/AHComposeDemo.kt
+++ sample/src/main/java/com/kiven/sample/compose/AHComposeDemo.kt
@@ -13,10 +13,14 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.kiven.kutils.activityHelper.KActivityHelper
 import com.kiven.kutils.activityHelper.KHelperActivity
@@ -112,9 +116,32 @@ fun TestContent(scaffoldState: ScaffoldState = rememberScaffoldState()) {
                     painter = painterResource(id = R.mipmap.ic_launcher_u),
                     contentDescription = ""
                 )
+
+                Text("padding(top = 24.dp)", Modifier.padding(top = 24.dp))
+                Text("firstBaselineToTop(24.dp)", Modifier.firstBaselineToTop(24.dp))
+                Text("paddingFrom(FirstBaseline, before = 24.dp)", Modifier.paddingFrom(FirstBaseline, before = 24.dp))
             }
         }
 
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = layout { measurable, constraints ->
+    // Measure the composable
+    val placeable = measurable.measure(constraints)
+
+    // Check the composable has a first baseline
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    // Height of the composable with padding - first baseline
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+        // Where the composable gets placed
+        placeable.placeRelative(0, placeableY)
     }
 }
 

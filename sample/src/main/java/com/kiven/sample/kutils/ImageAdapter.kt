8665fe36e579004e30ce13351af98d03ea0d2224
kiven <984768569@qq.com>
2022-06-15 16:12:52 +0800

    *

--- sample/src/main/java/com/kiven/sample/kutils/ImageAdapter.kt
+++ sample/src/main/java/com/kiven/sample/kutils/ImageAdapter.kt
@@ -1,6 +1,7 @@
 package com.kiven.sample.kutils
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.net.Uri
 import android.view.View
 import android.view.ViewGroup
@@ -10,28 +11,49 @@ import com.kiven.kutils.tools.KAppHelper
 import com.kiven.kutils.tools.KUtil
 import com.kiven.kutils.widget.UIGridView
 import com.kiven.sample.util.showImageDialog
+import java.io.File
 
 class ImageAdapter(private val itemWidth:Float = 100f, private val itemHeight:Float = 100f) : UIGridView.Adapter() {
-    private val data = mutableListOf<Uri>()
-    fun changeData(d: List<Uri>) {
+    private val data = mutableListOf<Any>()
+
+    /**
+     * 支持 Uri,String,File,Bitmap 等
+     */
+    fun changeData(d: List<Any>) {
         data.clear()
         data.addAll(d)
         notifyDataSetChanged()
     }
     override fun getGridViewItemCount() = data.size
     override fun getItemView(context: Context?, itemView: View?, parentView: ViewGroup?, position: Int): View {
-        return (itemView ?: ImageView(context)).also {
-            val iv = it as ImageView
+        return (itemView ?: ImageView(context)).also { iv ->
 
-            Glide.with(context!!)
-                .load(data[position])
-                .centerCrop()
+            val item  = data[position]
+
+            Glide.with(context!!).let {
+                when(item) {
+                    is Uri -> it.load(item)
+                    is String -> it.load(item)
+                    is File -> it.load(item)
+                    is Bitmap -> it.load(item)
+                    else -> it.load(item.toString())
+                }
+            }.centerCrop()
                 .error(android.R.drawable.ic_menu_report_image)
                 .override(KUtil.dip2px(itemWidth), KUtil.dip2px(itemHeight))
-                .into(iv)
+                .into(iv as ImageView)
 
             iv.setOnClickListener {
-                KAppHelper.getInstance().topActivity?.showImageDialog(data[position])
+                KAppHelper.getInstance().topActivity?.apply {
+
+                    when(item) {
+                        is Uri -> showImageDialog(item)
+                        is String -> showImageDialog(item)
+                        is File -> showImageDialog(item.path)
+                        is Bitmap -> showImageDialog(item)
+                        else -> showImageDialog(item.toString())
+                    }
+                }
             }
         }
     }

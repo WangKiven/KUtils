3c998cfd2790d503ad1ce7121697f8237f71de1a
kivenspc\wangkiven <984768569@qq.com>
2023-02-19 19:07:57 +0800

    添加app下载安装功能

--- kutils-ktx/src/main/java/com/sxb/kutils_ktx/util/KKUtil.kt
+++ kutils-ktx/src/main/java/com/sxb/kutils_ktx/util/KKUtil.kt
@@ -1,7 +1,13 @@
 package com.sxb.kutils_ktx.util
 
+import android.app.Activity
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
 import android.os.Parcel
 import android.os.Parcelable
+import com.kiven.kutils.callBack.Consumer
+import com.kiven.kutils.tools.KAppTool
 import kotlinx.coroutines.*
 
 /**
@@ -17,3 +23,35 @@ fun <T : Parcelable> createFromParcel(t: Class<T>, parcel: Parcel): T {
  * GlobalScope主线程调用
  */
 fun GlobalScope.main(block: suspend CoroutineScope.() -> Unit): Job = GlobalScope.launch(Dispatchers.Main, block = block)
+
+/**
+ * 调用系统下载功能下载apk并安装
+ * http://events.jianshu.io/p/ecb21640287b
+ * https://blog.csdn.net/zly19931101/article/details/89676772
+ */
+fun Activity.downloadApk(url: String, call: Consumer<Float>) {
+    val uri = Uri.parse(url)
+    val dm = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+//        dm.remove()
+    val request = DownloadManager.Request(uri)
+    val downloadId = dm.enqueue(request)
+
+    Thread {
+        while (true) {
+            Thread.sleep(5000)
+            val query = DownloadManager.Query().setFilterById(downloadId)
+            val cursor = dm.query(query) ?: break
+            if (cursor.moveToNext()) {
+                if (cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
+
+                    val f = dm.getUriForDownloadedFile(downloadId)
+                    runOnUiThread {
+                        KAppTool.installApk(this, f)
+                    }
+
+                    break
+                }
+            }
+        }
+    }.start()
+}
